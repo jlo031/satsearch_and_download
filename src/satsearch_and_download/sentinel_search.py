20658a7eb5a664a600f0ac8707f383337955f640
Johannes Lohse <johannes.p.lohse@uit.no>
2023-02-20 14:14:02 +0100

    updated module structure

--- src/satsearch_and_download/sentinel_search.py
+++ src/satsearch_and_download/sentinel_search.py
@@ -1,7 +1,7 @@
-# ---- This is <sentinel_search_and_download.py> ----
+# ---- This is <sentinel_search.py> ----
 
 """
-Module to facilitate search and batch download of Copernicus Sentinel data.
+Module to facilitate search of Copernicus Sentinel data.
 """ 
 
 import os
@@ -22,7 +22,7 @@ import satsearch_and_download.ssd_helpers as ssdh
 # -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
 
-def sentinel_search(
+def find_sentinel_products(
     sensors, 
     area, 
     starttime, 
@@ -34,20 +34,20 @@ def sentinel_search(
     loglevel = 'INFO'
 ):
     
-    """Search for Sentinel products
+    """Find Sentinel products from copernicus.scihub
 
     Parameters
     ----------
     sensors:
         list of Sentinel sensors to be included in the search (choices = ['S1', 'S2', 'S3'])
     area:
-        path to geojson file defining the area of interest (polygon)
+        path to geojson file defining the area of interest
     starttime:
         sensing start time 'YYYY-MM-DD HH:MM'
     endtime:
         sensing end time 'YYYY-MM-DD HH:MM'
     area_relation:
-        aree relation of polygon and products (choices = ['Contains', 'Intersects']
+        area relation of polygon and products (choices = ['Contains', 'Intersects']
     cloudcover:
         cloud cover percentage 'min,max' (default = '0,30')
     search_dir:
@@ -293,117 +293,9 @@ def sentinel_search(
 
 # -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
-
-def download_products_from_list(
-    product_list,
-    download_dir,
-    overwrite=False,
-    loglevel='INFO'
-):
-    """Download all products from product_list
-
-    Parameters
-    ----------
-    product_list : list with product name strings
-    download_dir : target directory for product download
-    overwrite : overwrite already existing products (download again)
-    """
-
-    # remove default logger handler and add personal one
-    logger.remove()
-    logger.add(sys.stderr, level=loglevel)
-
-
-    # count products
-    n_products = len(product_list)
-    logger.info(f'Found {n_products} products in product_list')
-
-    # convert folder strings to paths
-    download_dir = pathlib.Path(download_dir).expanduser().absolute()
-    logger.info(f'download_dir: {download_dir}')
-
-    # create download_dir if needed
-    download_dir.mkdir(parents=True, exist_ok=True)
-
-
-    # get creodias user and passwd
-    logger.debug('Loading environment variables from .env file')
-    load_dotenv()
-
-    try:
-        os.environ["CREO_USER"]
-    except:
-        logger.error("The environment variable 'CREO_USER' is not set.")
-        raise KeyError("The environment variable 'CREO_USER' is not set.")
-    
-    try:
-        os.environ["CREO_PASSWORD"]
-    except:
-        logger.error("The environment variable 'CREO_PASSWORD' is not set.")
-        raise KeyError("The environment variable 'CREO_PASSWORD' is not set.")
-
-
-
-    # initialize dictionary for download urls
-    S_dict = {}
-
-# -------------------------------------------------------------------------- #
-
-    # loop over all IDs in product list
-    for ID in product_list:
-
-        # check if product was already downloaded
-        if (download_dir / f"{ID}.zip").is_file() or (download_dir / f"{ID}.SAFE").is_dir():
-            if not overwrite:
-                logger.info(f'Product {ID}.zip already exists. Not downloading again.')
-                continue
-            else:
-                logger.info(f'Product {ID}.zip already exists. Overwriting existing file.')
-
-        # search Creodias to find Sentinel products from list
-        if ID.startswith('S1'):
-            creo_search_url = f'https://finder.creodias.eu/resto/api/collections/Sentinel1/search.json?maxRecords=10&productIdentifier=%25{ID}.SAFE%25&sortParam=startDate&sortOrder=descending&status=all&dataset=ESA-DATASET'
-            creo_search_results = ssdh.find_products(creo_search_url)
-            # extract corresponding creodias uid
-            uid = creo_search_results.json()['features'][0]['id']
-            # add uid-identifier as key-value pair to dictionary
-            S_dict[uid] = ID
-
-    if S_dict:
-        paths = ssdh.download_list(S_dict, os.environ["CREO_USER"], os.environ["CREO_PASSWORD"], download_dir)
-        logger.info('Batch download finished! See you next time:)')
-
-# -------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------- #
-
-def read_txt_file_to_list(txt_path):
-    """Read product IDs from txt file into list
-
-    Parameters
-    ----------
-    txt_path : path to txt file with product IDs
-
-    Returns
-    -------
-    product_list :  list with product name strings
-    """
-
-    # open file in read mode
-    with open(txt_path,'r') as f:
-        product_list = f.read().split("\n")
-
-    # remove potential empties
-    while("" in product_list):
-        product_list.remove("")
-
-
-    return product_list
-
-# -------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
 
-# ---- End of <sentinel_search_and_download.py> ----
+# ---- End of <sentinel_search.py> ----
 
